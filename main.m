fee426d6cb0d4b0e4eedbf03f06faf1baf1af518
wstcl <35589083+wstcl@users.noreply.github.com>
2018-07-12 15:28:00 -0700

    large initial(modified)

--- main.m
+++ main.m
@@ -1,7 +1,8 @@
-new_pop = population_init(8000);
+new_pop = population_init(40000);
 stop_sign = 0;
-pointer = 0;
-for Rounds=1:99999
+
+for Rounds=1:2
+    pointer = 0;
     for count=1:size(new_pop,1)
         pointer = pointer+1;
         cost(pointer)= fitness(new_pop(pointer,:));
@@ -11,21 +12,10 @@ for Rounds=1:99999
             pointer = pointer-1;
             continue
         end
-        if cost(pointer)==10
-            pass
-        end
-        if cost(pointer)==0
-            stop_sign = 1;
-            Table = Draw_table(new_pop(pointer,:))
-            break
-        end
+        
+        
     end
     
-    if stop_sign == 1
-        disp("Iteration Rounds =")
-        disp(Rounds)
-        break
-    end
     
     for i=1:2
         selected(i)=RouletteWheelSelection(cost);

a37a6a80c4960b3902fdaa2292bf599aed560a31
wstcl <35589083+wstcl@users.noreply.github.com>
2018-07-23 13:25:33 -0700

    Add files via upload

--- Draw_table.m
+++ Draw_table.m
@@ -6,23 +6,15 @@ date = ["Mon","Tue","Wed","Thr","Fri"];
 table = zeros(5,5,6);
 table = string(table);
 table(1,:,1)=date;
-
 for n=1:12
     room(n) = fix((code(n)-1)/20)+1;
     %rm(n) = strcat("classroom",num2str(room));
-    slot(n) = mod(code(n)-1,20)+1;%time information for subject n
-    colum(n) = mod(slot(n)-1,5)+1;%date
-    row(n) = fix((slot(n)-1)/5)+2;%
+    time(n).slot = mod(code(n)-1,20)+1;%time information for subject n
+    time(n).colum = mod(time(n).slot-1,5)+1;
+    time(n).row = fix((time(n).slot-1)/5)+2;
     %data(n)=strcat(rm(n),subject(n));%room and subject for subject n
-    table(row(n),colum(n),room(n))=subject(n);
-
+    table(time(n).row,time(n).colum,room(n))=subject(n);
 end
-figure
-stem3(row-1,colum,room)
-xlabel('time')
-ylabel('date')
-zlabel('room')
-hold on
 t=table;
 
 
--- main.m
+++ main.m
@@ -1,32 +1,19 @@
-new_pop = population_init(1000);
-cost_sum = 0;
-for Rounds=1:20
-    pointer = 0;
-    for count=1:size(new_pop,1)
-        pointer = pointer+1;
-        cost(pointer)= fitness(new_pop(pointer,:));
-        if cost(pointer)>=100
-            new_pop(pointer,:)=[];
-            cost(pointer)=[];
-            pointer = pointer-1;
-            continue
+new_pop = population_init();
+stop_sign = 0;
+for j=1:500000
+    for i=1:20
+        cost(i)= fitness(new_pop(i,:));
+        if cost(i)==0
+            stop_sign = 1;
+            Draw_table(new_pop(i,:))
+            break
         end
     end
-    cost_sum(Rounds) = sum(cost);
+    cost_sum(j) = sum(cost);
     
-    
-    if isempty(cost)
-        new_pop = population_init(1000);
-        cost_sum(Rounds) = -1;
-        continue
-    end
-    
-    if cost == zeros(size(cost))
-        Draw_table(new_pop(1,:))
+    if stop_sign == 1 
         break
     end
-    
-    
     
     for i=1:2
         selected(i)=RouletteWheelSelection(cost);
@@ -37,10 +24,8 @@ for Rounds=1:20
     new_pop(selected(1),:)=cross1;
     new_pop(selected(2),:)=cross2;
 end
-figure 
-xlabel('Number of iterations')
-ylabel('Summation of cost for pop')
-X = 1:Rounds;
-scatter(X,cost_sum,'filled')
+plot(cost_sum)
+xlabel('Iteration')
+ylabel('Population Cost')
 hold on
 

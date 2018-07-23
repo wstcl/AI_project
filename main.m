b418b34ec1f7d03ed7dc9a3ddae82cc78a8e9c4e
wstcl <35589083+wstcl@users.noreply.github.com>
2018-07-23 13:23:11 -0700

    Add files via upload

--- main.m
+++ main.m
@@ -1,18 +1,32 @@
-new_pop = population_init();
-stop_sign = 0;
-for j=1:100000
-    for i=1:20
-        cost(i)= fitness(new_pop(i,:));
-        if cost(i)==0
-            stop_sign = 1;
-            Draw_table(new_pop(i,:))
-            break
+new_pop = population_init(1000);
+cost_sum = 0;
+for Rounds=1:20
+    pointer = 0;
+    for count=1:size(new_pop,1)
+        pointer = pointer+1;
+        cost(pointer)= fitness(new_pop(pointer,:));
+        if cost(pointer)>=100
+            new_pop(pointer,:)=[];
+            cost(pointer)=[];
+            pointer = pointer-1;
+            continue
         end
     end
+    cost_sum(Rounds) = sum(cost);
     
-    if stop_sign == 1 
+    
+    if isempty(cost)
+        new_pop = population_init(1000);
+        cost_sum(Rounds) = -1;
+        continue
+    end
+    
+    if cost == zeros(size(cost))
+        Draw_table(new_pop(1,:))
         break
     end
+    
+    
     
     for i=1:2
         selected(i)=RouletteWheelSelection(cost);
@@ -23,4 +37,10 @@ for j=1:100000
     new_pop(selected(1),:)=cross1;
     new_pop(selected(2),:)=cross2;
 end
+figure 
+xlabel('Number of iterations')
+ylabel('Summation of cost for pop')
+X = 1:Rounds;
+scatter(X,cost_sum,'filled')
+hold on
 
